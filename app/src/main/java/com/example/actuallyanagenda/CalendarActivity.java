52883a96970c88b1ea352b010ca58743b04a25d8
yeahbennou <benzeng0506@gmail.com>
2021-07-25 08:36:00 -0400

    Add all schedule-building algorithms

---
+++ app/src/main/java/com/example/actuallyanagenda/CalendarActivity.java
@@ -0,0 +1,28 @@
+package com.example.actuallyanagenda;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.CalendarView;
+import android.widget.ListView;
+
+public class CalendarActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_calendar);
+
+        CalendarView calendarView = findViewById(R.id.calendarView);
+        ListView listView = findViewById(R.id.list);
+//        listView.
+    }
+
+    /**
+     * Adds an entry to the calendar using
+     * @param entry
+     */
+    protected void addEntry(CalendarEntry entry) {
+
+    }
+}
